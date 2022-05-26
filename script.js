16fa4147a87ed24e6099339406e7e0fc636733a3
Misha535655 <visvan3333@gmail.com>
2022-05-26 19:20:19 +0300

    add films json

--- script.js
+++ script.js
@@ -1,2 +1,57 @@
-console.log('aaa');
-console.log('sdadsa');
+let Ghostbusters = [{
+        'Name': 'Ghostbusters',
+        'Ratings': 7.8,
+        'Tagline': `Someone is fighting cockroaches and insects. They're cleaning the city of ghosts.`,
+        'Release': 'June 7, 1984',
+        'Side': 'USA',
+        'Producer': 'Ivan Reitman',
+        'Genre': 'Science Fiction , Fantasy , Action , Comedy , Foreign',
+        'Age': '16+ for more mature and understanding',
+        'Time': '105 min.',
+        'description': `Bill, Dan, Harold and Ernie worked as "paranormal investigators" in Manhattan until the government grants allocated for the fight against evil spirits ran out. After some time, bosom friends decide to organize the Ghostbusters service, which quickly begins to be in demand, successfully ridding New Yorkers of ghouls, ghosts and ghosts. However, William Atherton, a hardened bureaucrat, believes the crew to be charlatans, but is forced to ask for the hunters' help when an army of hostile spirits attacks the city.`
+    },
+    {
+        'Name': 'Ghostbusters 2',
+        'Ratings': 6.6,
+        'Tagline': `The ghosts have returned... and in vain!`,
+        'Release': 'June 15, 1989',
+        'Side': 'USA',
+        'Producer': 'Ivan Reitman',
+        'Genre': 'Science Fiction , Action , Fantasy , Comedy , Adventure , Foreign',
+        'Age': '12+ adult themes and concepts, but the topics are not covered',
+        'Time': '108 min',
+        'description': `The actors in this film look great, their performance captures and takes away with them. As if you are transferred to the screen and, together with the hunters, you save the world from scary and not very scary ghosts. In the first part, the magnificent four hunters famously dealt with Gozer and for five years nothing has happened. Ghosts hid, and the glory and exploits of friends were forgotten. Egon, Ray, Peter and Winston live ordinary lives, each busy with something different. But the threat loomed over the city again. In one of the museums in New York, a very suspicious picture appears in which the spirit comes to life. And in the sewers under the city, some kind of pink slime is spreading, giving rise to demons. The hunters are back in business, they have to save their hometown from raging ghosts and restore calm to the streets of New York. Ghostbusters won the love of millions of viewers. We see them in cartoons, movies, comics. They are favorite characters of the 80s and 90s. They are bold, interesting, confident, funny, just the way heroes should be.`
+    },
+    {
+        'Name': 'Ghostbusters',
+        'Ratings': 6.9,
+        'Tagline': `Who will you call for help?`,
+        'Release': 'July 9, 2016',
+        'Side': 'USA , Australia',
+        'Producer': 'Paul Fig',
+        'Genre': 'Science Fiction , Fantasy , Action , Comedy , Foreign',
+        'Age': '16+ for more mature and understanding',
+        'Time': '116 min.',
+        'description': `The remake of the popular 80s blockbuster will focus on paranormal researcher Abby Yates and physicist Erin Gilbert as they try to prove that ghosts really exist. When mysterious events begin to unfold in Manhattan, Gilbert and Yates turn to nuclear engineer Gillian Holtzman for help. Also joining the team is subway worker Patty Tolan, who knows the city inside and out. Armed with proton backpacks and unwavering self-confidence, four women prepare for an epic battle against the army of the dead...`
+    },
+    {
+        'Name': 'Ghostbusters: Afterlife',
+        'Ratings': 7.1,
+        'Tagline': `If there's something strange in your neighborhood, Then you know who to call.`,
+        'Release': 'October 8, 2021',
+        'Side': 'USA , Canada',
+        'Producer': 'Jason Reitman',
+        'Genre': 'Sci -Fi , Fantasy , Comedy , Adventure , Action , Foreign',
+        'Age': '12+ adult themes and concepts, but the topics are not covered',
+        'Time': '124 min.',
+        'description': `Single mother Kelly, burdened with financial troubles, along with teenage children Phoebe and Trevor, moves to a provincial town in Oklahoma, where her late father's dilapidated farm is located. Soon an inquisitive Phoebe, exploring the nooks and crannies of their new home, unexpectedly finds old equipment to destroy ghosts. With the help of Mr. Groberson, a science teacher at a local school, Phoebe and Trevor discover the truth about the exploits of ghost hunters in the 1980s, in which their grandfather was actively involved. When paranormal events begin to occur in a provincial town, the main characters reveal grim details about their family's past.`
+    },
+]
+
+
+let toggleMenu = document.getElementById('toggleMenu');
+
+function toggleMenuClick() {
+    toggleMenu.className.includes('is-active') ? toggleMenu.classList.remove('is-active') : toggleMenu.classList.add('is-active')
+}
+toggleMenu.addEventListener('click', () => toggleMenuClick())
